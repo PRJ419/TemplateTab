261cf1bb93e4747aeb440e71f8230a870f846b1c
Welle77 <36986182+Welle77@users.noreply.github.com>
2019-02-28 12:40:21 +0100

    Add project files.

---
+++ TemplateTab/TemplateTab/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TemplateTab.Models;
+
+namespace TemplateTab.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
